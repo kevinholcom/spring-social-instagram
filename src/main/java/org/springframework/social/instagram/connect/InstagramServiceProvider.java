3353f3972491fa43017faa2f7e6e05c9848dac11
juan.torres <juan.torres@pragma.com.co>
2017-07-12 19:11:09 -0500

    Fixed Url form encoding issues on Oauth2 callback

--- src/main/java/org/springframework/social/instagram/connect/InstagramServiceProvider.java
+++ src/main/java/org/springframework/social/instagram/connect/InstagramServiceProvider.java
@@ -3,14 +3,22 @@ package org.springframework.social.instagram.connect;
 import org.springframework.social.instagram.api.Instagram;
 import org.springframework.social.instagram.api.impl.InstagramTemplate;
 import org.springframework.social.oauth2.AbstractOAuth2ServiceProvider;
+import org.springframework.social.oauth2.OAuth2Template;
 
 public class InstagramServiceProvider extends AbstractOAuth2ServiceProvider<Instagram> {
 
 	private final String clientId;
 
 	public InstagramServiceProvider(String clientId, String clientSecret) {
-		super(new InstagramOAuth2Template(clientId, clientSecret));
+		super(getOAuth2Template(clientId, clientSecret));
 		this.clientId = clientId;
+	}
+	
+	private static OAuth2Template getOAuth2Template(String clientId, String clientSecret) {
+		OAuth2Template oAuth2Template = new OAuth2Template(clientId, clientSecret,
+				"https://api.instagram.com/oauth/authorize", "https://api.instagram.com/oauth/access_token");
+		oAuth2Template.setUseParametersForClientAuthentication(true);
+		return oAuth2Template;
 	}
 
 	public Instagram getApi(String accessToken) {
